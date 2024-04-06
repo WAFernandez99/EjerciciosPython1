903554aaea57dbef8fc3981034740de1b7abc561
WAFernandez99 <126583003+WAFernandez99@users.noreply.github.com>
2024-04-06 02:52:27 +0000

    Primeras actividades

--- carpeta sin título/prueba.py
+++ carpeta sin título/prueba.py
@@ -1,14 +1,38 @@
-print('Hello World')
-RL= float(input('Ingrese rendimiento del litro x m2'))
-M= int(input('Ingrese la cantidad de manos que desea dar'))
-L= float(input('Ingrese el largo en metros'))
-An= float(input('Ingrese el Ancho en metros'))
-A= float(input('Ingrese el Alto en metros'))
-Area= ((L * A)* 2) + ((An * A)* 2)
-T= L * An
-BT= Area + T
-BTM= BT * M
-Res= BTM / RL
-print('Usted necesita', Res ,'litros')
+#print('Hello World')
+#RL= float(input('Ingrese rendimiento del litro x m2'))
+#M= int(input('Ingrese la cantidad de manos que desea dar'))
+#L= float(input('Ingrese el largo en metros'))
+#An= float(input('Ingrese el Ancho en metros'))
+#A= float(input('Ingrese el Alto en metros'))
+#Area= ((L * A)* 2) + ((An * A)* 2)
+#T= L * An
+#BT= Area + T
+#BTM= BT * M
+#Res= BTM / RL
+#print('Usted necesita', Res ,'litros')
 
 
+#Actividad 1 de operaciones basicas
+#A= 186
+#B= 24
+#Suma= A + B
+#Resta= A - B
+#Multi= A * B
+#Div= A / B
+#Dive= A // B
+#Resto= A % B
+#Lista= Suma, Resta,Multi, Div,Dive, Resto
+#print('El resultado de suma,resto, multi, div, dive, resto se mostrara respesctivamente en ese orden.', Lista)
+
+#Actividad 2 
+#A= 16823
+#UD= A % 10
+#DD= A % 100
+#TD= A % 1000
+#CD= A// 10
+#PD= A // 10000
+#PDD= A // 1000
+#Lista= ('a',UD, 'B',DD, 'C', TD, 'D',CD, 'E',PD, 'F',PDD,)
+#print('Resultado',Lista)
+
+#Actividad 3
